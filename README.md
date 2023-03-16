d0a97ef7f3c17756c826ecde23f1061e1871fb08
Vincent Reid <vreid@tangiblink.io>
2023-03-16 17:55:57 +0000

    Updated API function to return an int value to help reduce gas costs, function 'words_to_coordinates' now changed to 'words_check' with main purpose to confirm that the words are a correct W3W address

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # W3W-API
+***
+## API Purpose
+This API has been created to enable cheching of incoming data for correct format and then subsequently calling the What3Words API to retreive data for return/ comparison.
+---
+## Resources
+### API Key
+API Key can be obtained from here - [What3Words API KEY](https://accounts.what3words.com/create-api-key)
+### Example https GET addresses
+* Words convert to coordinates: https://api.what3words.com/v3/convert-to-coordinates?words=filled.count.soap&key=[API-KEY]
+* Coordinates to Words: https://api.what3words.com/v3/convert-to-3wa?coordinates=51.521251%2C-0.203586&key=[API-KEY]
+
+## Terms of Use
+What3Words requires that no API data is stored in relation to a 3 Words Address. However unlimited 3 Words addresses my be stored. 
+
+## API Requirements
+* The API must be designed so that results from the Words to Coordinates calls are not returning results on a blockchain as this would store data in transaction data, emitted events and potentially storage variables.
+* Results provided to Nodes can be as follows:
+    1. Integar (To confirm Words are correct)
+    3. Details of near by squares, surrounding Squares, nearest Place, country
+    4. Distance between 2 locations by Lng and Lat (Haversine) (Parameters - Squares or coordinates)
+
+# API functions
+***
+## Function name: check_words
+## URL: /check_words
+Results are returned as an integar with the "result" key value pair. This can be processed at a lower gas fee than a Bool or String.
+### Results identifiers:
+1. result: 1 = "Words address is correct"
+2. result: 2 = "Words address does not exist"
+3. result: 3 = "Incorrect number of words provided"
+4. result: 4 = "Incorrect number of arguments provided. Words to be provided as a single string seperated by punctuation mark '.' e.g. "prom.cape.pump:".
+5. result: 5 = "Incorrect argument type provided"
+6. result: 6 = "Fatal error"
--- main.py
+++ main.py
@@ -1,39 +1,68 @@
 import what3words
 import os
-from flask import Flask, request, url_for, redirect, abort
+from flask import Flask, request, url_for, redirect, abort, jsonify
 
 api_key = os.environ.get("API_KEY")
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def index():
 
-    if len(request.args) == 1:
-        location_words = request.args["words"]
-        location_check = location_words.split(".")
-        if len(location_check) == 3:
-            return redirect(url_for("words_to_coordinates", words=location_words))
+    return "Check out Tangiblink.io for info on this API"
 
-    abort(400)
-
-@app.route("/words_to_coordinates")
-def words_to_coordinates():
+@app.route("/words_check", methods=["GET"])
+def words_check():
 
     if len(request.args) == 1:
-        location_3_words = request.args["words"]
+        if "words" in request.args:
+            location_words = request.args["words"]
+            location_check = location_words.split(".")
+            for word in location_check:
+                if len(word) == 0:
+                    return jsonify(result=3)
+            if len(location_check) == 3:
+                # API Key addded in API call variable.
+                geocoder = what3words.Geocoder(api_key)
 
-        # API Key addded in API call variable.
-        geocoder = what3words.Geocoder(api_key)
+                contents = geocoder.convert_to_coordinates(location_words)
 
-        contents = geocoder.convert_to_coordinates(location_3_words)
+                if "error" in contents:
+                    return jsonify(result=2)
 
-        if "error" in contents:
-            abort(400)
-
-        return contents
+                elif location_words == contents["words"]:
+                    return jsonify(result=1)
+                
+                else:
+                    return jsonify(result=6)
+            
+            if len(location_check) != 3:
+                return jsonify(result=3)
+        else:
+            return jsonify(result=5)
+        
+    elif len(request.args) != 1:
+        return jsonify(result=4)
     
-    abort(400)
+    return jsonify(result=6)
+
+# words check results:
+#  result: 1 = "words address is correct"
+#  result: 2 = "words address does not exist"
+#  result: 3 = "incorrect number of words provided"
+#  result: 4 = "incorrect number of arguments provided. Words to be provided as a single string seperated by punctuation mark '.' e.g. "prom.cape.pump:".
+#  result: 5 = "incorrect argument type provided"
+#  result: 6 = "some other fatal error"
+
+# Coordinates to Words - Return Words
+
+# Bulk coordinates to words
+
+# Words - Get surrounding Square words (CENTER, N, NE, E, SE, S, SW, W, NW)
+
+# Square size in micrometers width x height
+
+# Haversine - Distance between 2 points
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8000, debug=True)
